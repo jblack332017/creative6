f633de5e95e684e45cda5827a900f4cc63d920bf
Josh Black <joshblack@tigger.local>
2017-04-17 16:46:17 -0600

    eleet

--- hackChallenge/routes/index.js
+++ hackChallenge/routes/index.js
@@ -62,8 +62,22 @@ router.get('/user/:email', function(req,res,nex) {
 					});
 					
 				}
-
-
+				jsonObj = [];
+				item = {};
+				if (userInfo.one=="yes")
+				{
+					item["one"] = "<a href='challenge1.html' class='w3-btn'>One</a>"
+				}
+				if (userInfo.two=="yes")
+				{
+					item["two"] = "<a href='challenge2.html' class='w3-btn'>Two</a>"
+				}
+				if (userInfo.three=="yes")
+				{
+					item["three"] = "<a href='challenge3.html' class='w3-btn'>Three</a>"
+				}
+				jsonObj.push(item);
+				res.json(jsonObj);
     
     		}
 			  
