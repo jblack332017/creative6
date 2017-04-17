cfb62ddca87320dc76d81636bba695e3746a38ef
Josh Black <joshblack@byu630113wks.app.byu.edu>
2017-04-17 15:28:42 -0600

    eleet

--- hackChallenge/routes/index.js
+++ hackChallenge/routes/index.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/challenge');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
+var userSchema = mongoose.Schema({
+    email: String,
+    one: String,
+    two: String,
+    three: String,
+});
+
+var User = mongoose.model('Review', userSchema);
+
+var silence = new User({ email: 'Silence',one: 'no' });
+console.log(silence.one); // 'Silence'
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
