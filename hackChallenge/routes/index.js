06b8330a577968def52b3cba4762de9a2923d055
Josh Black <joshblack@tigger.local>
2017-04-17 22:36:05 -0600

    eleet

--- hackChallenge/routes/index.js
+++ hackChallenge/routes/index.js
@@ -47,6 +47,7 @@ router.get('/mywayout/:email', function(req, res, next) {
   	User.findOne({ email: req.params.email }, function (err, doc){
   doc.three = 'yes';
   doc.save();
+  res.json(doc);
 });
 
 
